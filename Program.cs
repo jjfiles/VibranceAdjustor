24981fd216a472b6575c55b86420efab1dd5a8e3
jjfiles <jeffery@jfiles.dev>
2022-08-17 02:47:39 -0500

    updated to use settings file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text.Json;
 using NvAPIWrapper;
 using NvAPIWrapper.Display;
 
@@ -9,34 +10,35 @@ internal class Program
     private static void Main()
     {
         NVIDIA.Initialize();
+        Settings settings = LoadSettings();
         var displays = Display.GetDisplays();
         var mainDisplay = displays[0];
-        // TODO: setting for EXE name
-        if (Process.GetProcessesByName("VALORANT").Length > 0)
+        if (Process.GetProcessesByName(settings.Application.Name).Length > 0)
         {
-            // TODO: replace with exe name
-            Console.WriteLine("VALORANT detected");
-            // TODO: replace max value
+            Console.WriteLine($"{settings.Application.Name} detected");
             mainDisplay.DigitalVibranceControl.CurrentLevel = 100;
-            Console.WriteLine("Vibrance set");
+            Console.WriteLine($"Vibrance set to {settings.Vibrance.Max}");
             
-            // TODO option for opening obs
-            if (Process.GetProcessesByName("obs64").Length == 0)
+            if (Process.GetProcessesByName("obs64").Length == 0 && settings.OBS.Enable)
             {
                 Console.WriteLine("Starting OBS");
-                // TODO: add path from settings
-                ProcessStartInfo si = new ProcessStartInfo(@"B:\obs-studio\bin\64bit\obs64") {WorkingDirectory = @"B:\obs-studio\bin\64bit"};
+                ProcessStartInfo si = new ProcessStartInfo(Path.Combine(settings.OBS.Folder, settings.OBS.Name)) {WorkingDirectory = $"{settings.OBS.Folder}"};
                 Process.Start(si);
             }
         }
         else
         {
-            // TODO: replace with app name
-            Console.WriteLine("VALORANT not detected");
-            // TODO: let user set default level
-            mainDisplay.DigitalVibranceControl.CurrentLevel = mainDisplay.DigitalVibranceControl.DefaultLevel;
-            Console.WriteLine("Setting vibrance to default");
+            Console.WriteLine($"{settings.Application.Name} not detected");
+            mainDisplay.DigitalVibranceControl.CurrentLevel = settings.Vibrance.Normal;
+            Console.WriteLine($"Setting vibrance to {settings.Vibrance.Normal}");
         }
+    }
+
+    private static Settings LoadSettings()
+    {
+        string text = File.ReadAllText(@"userSettings.json");
+        return JsonSerializer.Deserialize<Settings>(text);
+        
     }
 }
 
---
+++ Settings.cs
@@ -0,0 +1,27 @@
+﻿namespace VibranceAdjustor;
+
+public class Application
+{
+    public string Name { get; set; }
+}
+
+public class OBS
+{
+    public string Name { get; set; }
+    public string Folder { get; set; }
+    public bool Enable { get; set; }
+}
+
+public class Settings
+{
+    public Application Application { get; set; }
+    public Vibrance Vibrance { get; set; }
+    public OBS OBS { get; set; }
+}
+
+public class Vibrance
+{
+    public int Max { get; set; }
+    public int Normal { get; set; }
+}
+
