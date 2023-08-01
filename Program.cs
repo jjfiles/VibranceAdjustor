ed4bdc53f04bef27ed2d2a513eb0fb3dd51384b2
jjfiles <jeffery@jfiles.dev>
2023-08-01 18:43:06 -0500

    some stuff

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ internal class Program
 {
     private static void Main()
     {
-        // TODO: test
         // TODO: release
         NVIDIA.Initialize();
         Settings settings = LoadSettings();
